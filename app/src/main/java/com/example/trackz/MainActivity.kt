8ddb5b1347c52689b30d29ec94af12794c0a1461
sebnow91 <sebnow91@gmail.com>
2024-05-08 10:40:20 +0200

    Add navigation, basic tablet layout

--- app/src/main/java/com/example/trackz/MainActivity.kt
+++ app/src/main/java/com/example/trackz/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.trackz
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -18,12 +19,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.trackz.components.Title
-import com.example.trackz.components.TrackList
+import com.example.trackz.components.TrackDescription
+import com.example.trackz.components.TrackListMobile
+import com.example.trackz.components.TrackListTablet
+import com.example.trackz.components.Tracks
 import com.example.trackz.components.WindowInfo
 import com.example.trackz.components.rememberWindowInfo
 import com.example.trackz.ui.theme.TrackzTheme
@@ -31,8 +39,12 @@ import com.example.trackz.ui.theme.TrackzTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         setContent {
             TrackzTheme {
+                var name by remember {
+                    mutableStateOf(Tracks.keys.first())
+                }
                 val windowInfo = rememberWindowInfo()
                 // A surface container using the 'background' color from the theme
                 if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Medium) {
@@ -42,7 +54,7 @@ class MainActivity : ComponentActivity() {
                     ) {
                         Column {
                             Title("Trackz")
-                            TrackList()
+                            TrackListMobile()
                         }
                     }
                 } else {
@@ -50,11 +62,21 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background
                     ) {
-                        Column()
-                        {
-                            Title("Trackz")
-                            TrackList()
-
+                        Row {
+                            Column(
+                                modifier = Modifier.weight(1f)
+                            )
+                            {
+                                Title("Trackz")
+                                TrackListTablet(Tracks, onTrackClick = { selectedTrack ->
+                                    name = selectedTrack
+                                })
+                            }
+                            Column(
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                TrackDescription(track = name)
+                            }
                         }
                     }
                 }
@@ -74,7 +96,7 @@ fun GreetingPreview() {
             {
                 Column{
                     Title("Trackz")
-                    TrackList()
+                    TrackListMobile()
                 }
             }
     }
--- app/src/main/java/com/example/trackz/components/Tracks.kt
+++ app/src/main/java/com/example/trackz/components/Tracks.kt
@@ -1,17 +1,26 @@
 package com.example.trackz.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
 
 var Tracks = hashMapOf<String, String>(
@@ -29,16 +38,79 @@ var Tracks = hashMapOf<String, String>(
     )
 
 @Composable
-fun TrackList(){
+fun TrackListMobile(tracks: HashMap<String, String> = Tracks){
     LazyColumn {
         items(Tracks.keys.toList()) { track ->
             Text(
                 text = track,
                 color = MaterialTheme.colorScheme.primary,
                 fontSize = 30.sp,
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
             )
             Divider(thickness = 2.dp)
         }
     }
+}
+
+@Composable
+fun TrackListTablet(tracks: HashMap<String, String> = Tracks, onTrackClick: (String) -> Unit){
+    LazyColumn {
+        items(Tracks.keys.toList()) { track ->
+            Text(
+                text = track,
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 30.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .clickable { onTrackClick(track) }
+            )
+            Divider(thickness = 2.dp)
+        }
+    }
+}
+
+@Composable
+fun TrackDescription(track: String){
+    Column {
+        Text(text = track,
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 50.sp,
+            modifier = Modifier.padding(16.dp))
+        Divider(thickness = 5.dp, color = MaterialTheme.colorScheme.secondary)
+        Text(
+            text = Tracks[track]!!,
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun MainScreenMobile(navController: NavController) {
+    var name by remember {
+        mutableStateOf(Tracks.keys.first())
+    }
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            Title("Trackz")
+            TrackListMobile()
+        }
+    }
+}
+
+@Composable
+fun TrackDescriptionMobile(navController: NavController, track: String) {
+    Text(
+        text = Tracks[track]!!,
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = 20.sp,
+        modifier = Modifier.padding(16.dp)
+    )
 }
